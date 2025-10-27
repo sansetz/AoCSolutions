b17c4f775938b8fed90c37ec7b7a5b1fb0ad0c8c
Sandra Setz <ssetz75@gmail.com>
2025-10-27 20:34:37 +0100

    Finished First puzzle

--- SonarSweepPuzzle/Program.cs
+++ SonarSweepPuzzle/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+List<int> measurements = new List<int>() { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+
+Console.WriteLine(CountMeasurementIncreases(measurements));
+
+int CountMeasurementIncreases(List<int> measurements) {
+    int count = 0;
+    for (int i = 1; i < measurements.Count; i++) {
+        if (measurements[i] > measurements[i - 1]) {
+            count++;
+        }
+    }
+    return count;
+}
